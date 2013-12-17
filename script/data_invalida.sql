7c191adc23cc62e81cec6697762aabb3642ab61c
Carlos Silveira <carlos@atsti.com.br>
2013-12-17 16:53:21 -0200

    Não permitindo data menor q 6 meses.

---
+++ script/data_invalida.sql
@@ -0,0 +1,1 @@
+CREATE EXCEPTION data_invalida 'Data menor que o permitido.';
---
+++ script/data_invalida_venda.sql
@@ -0,0 +1,9 @@
+CREATE TRIGGER data_invalida_venda FOR VENDA
+ACTIVE BEFORE INSERT OR UPDATE POSITION 0
+AS 
+BEGIN 
+	IF (new.DATAVENDA < (current_date - 250)) then 
+	  exception data_invalida;
+	IF (new.DATAVENCIMENTO < (current_date - 250)) then 
+	  exception data_invalida;	  
+END
