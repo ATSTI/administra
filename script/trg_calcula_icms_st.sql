1b506fc1e18141cf6616f1a84abf0b61b1fa1586
Carlos Silveira <carlos@atsti.com.br>
2014-09-15 12:31:48 -0300

    Buscando tributação corretamente.

--- script/trg_calcula_icms_st.sql
+++ script/trg_calcula_icms_st.sql
@@ -1,5 +1,4 @@
-set term ^ ;
-CREATE OR ALTER TRIGGER CALCULA_ICMS_ST ACTIVE
+ALTER TRIGGER CALCULA_ICMS_ST ACTIVE
 BEFORE INSERT OR UPDATE POSITION 0
 AS
  DECLARE VARIABLE UF char(2);
@@ -28,6 +27,7 @@ AS
  DECLARE VARIABLE PIS DOUBLE PRECISION = 0;
  DECLARE VARIABLE COFINS DOUBLE PRECISION = 0;
  Declare variable IE varchar (24);
+ Declare variable CFOP_MOV varchar (30);
  Declare variable CSTIPI varchar(2);
  Declare variable CSTPIS varchar(2); 
  Declare variable CSTCOFINS varchar(2); 
@@ -37,18 +37,56 @@ AS
  DECLARE VARIABLE aliqimp DOUBLE PRECISION;
  Declare variable NCM_P varchar(8);
  Declare variable origem integer;
+ Declare variable NATUREZA integer;
  Declare variable CALCTRIB varchar(1); 
+ Declare variable UF_EMPRESA char(2); 
  DECLARE VARIABLE TOTALITENS DOUBLE PRECISION;
 BEGIN
-  -- versao 1.2.2
+  -- versao 3.0.0.0
   IF ((NEW.PAGOU IS NULL) or (new.PAGOU <> 'M')) THEN  -- Calculo manual 
   begin 
+  
+    select first 1 emp.UF from EMPRESA emp
+      into :UF_EMPRESA; 
+  
+	select FIRST 1 CRT from EMPRESA , MOVIMENTO where CODALMOXARIFADO = CCUSTO and CODMOVIMENTO = new.CODMOVIMENTO
+	  into :CRT;  
+
+	select FIRST 1 CODNATUREZA, CASE WHEN CODNATUREZA = 3 THEN CODCLIENTE WHEN CODNATUREZA = 4 THEN CODFORNECEDOR
+	 ELSE 0 end from MOVIMENTO where CODMOVIMENTO = new.CODMOVIMENTO
+	  into :NATUREZA, :CODCLI; 
+	  
+	if (natureza in (1,4)) then 
+	begin 
+      select first 1 ef.UF, f.CODFISCAL, f.INSCESTADUAL from FORNECEDOR f
+	   left outer join ENDERECOFORNECEDOR ef on ef.CODFORNECEDOR = f.CODFORNECEDOR
+	   where f.CODFORNECEDOR = :codCli
+	     and ef.TIPOEND = 0 
+	    into :UF, :PESSOA, :IE;
+	end   
+
+	if (natureza in (2,3)) then 
+	begin 
+      select first 1 ec.UF, c.CODFISCAL, c.INSCESTADUAL from Clientes c
+	   left outer join ENDERECOCLIENTE ec on ec.CODCLIENTE = c.CODCLIENTE
+	   where c.CODCLIENTE = :codCli
+	     and ec.TIPOEND = 0 
+	    into :UF, :PESSOA, :IE;
+	end  
+	select p.NCM from produtos p where p.CODPRODUTO = new.CODPRODUTO
+	  into :NCM_P;  
+  
+	if ((new.cfop = '') or (new.CFOP is null)) then
+	begin
+      select first 1 bc.CFOP_MOV from busca_cfop(:UF, :UF_EMPRESA, :NATUREZA, :NCM_P, new.CODPRODUTO, :PESSOA) bc
+        into :cfop_mov;        
+      new.CFOP = :cfop_mov;   
+    end    
+
 	if ( (new.cfop = '') or (new.CFOP is null) ) then
-            select first 1 cfp.CFOP from CLASSIFICACAOFISCALPRODUTO cfp 
-            where cfp.COD_PROD = new.CODPRODUTO
-            into new.CFOP;
-	if ( (new.cfop = '') or (new.CFOP is null) ) then
-		new.cfop = '5102';
+	begin 
+	  new.cfop = '5102';
+	end 	
 			
     if ((new.CFOP <> '') or ((updating) and ((new.QTDE_ALT <> old.QTDE_ALT) or (new.PRECO <> old.PRECO) or (new.QUANTIDADE <> old.QUANTIDADE)))) then 
     begin
@@ -62,9 +100,6 @@ BEGIN
 			
         if (UDF_LEFt(new.cfop,1) = '1') then 
           arredondar = 6;
-          
-		select FIRST 1 CRT from EMPRESA, MOVIMENTO where CODALMOXARIFADO = CCUSTO and CODMOVIMENTO = new.CODMOVIMENTO
-		into :CRT;
 
 		if (new.QTDE_ALT is null) then 
 			new.QTDE_ALT = 0;
@@ -85,11 +120,6 @@ BEGIN
 		new.VLR_BASEICMS = 0;
 		new.VALOR_ICMS = 0;
 
-		select first 1 ec.UF, c.CODFISCAL, m.CODCLIENTE, c.INSCESTADUAL from movimento m
-		inner join ENDERECOCLIENTE ec on ec.CODCLIENTE = m.CODCLIENTE
-		inner join CLIENTES c on c.CODCLIENTE = m.CODCLIENTE
-		where ec.TIPOEND = 0 and m.CODMOVIMENTO = new.CODMOVIMENTO
-		into :UF, :PESSOA, :CODCLI, :IE;
 	
 		if (:CODCLI is null) then
 		begin
@@ -581,4 +611,4 @@ BEGIN
       
 	new.VLR_BASE = UDF_ROUNDDEC(new.PRECO, :arredondar);  
   end
-END ^
+END
