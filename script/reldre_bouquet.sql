fc6603d5f1a3b511e1b60d660b92e87672a55300
Carlos Silveira <carlos@atsti.com.br>
2014-09-15 12:30:44 -0300

    fixup! fixup! fixup! Sem descrição

--- script/reldre_bouquet.sql
+++ script/reldre_bouquet.sql
@@ -11,11 +11,15 @@ RETURNS (
     ACUMULA DOUBLE PRECISION,
     TOT CHAR(1) )
 AS
-DECLARE VARIABLE CODREC INTEGER;
+  DECLARE VARIABLE CODREC INTEGER;
+  DECLARE VARIABLE CODP INTEGER;  
   DECLARE VARIABLE CENTROPERDA INTEGER;
   DECLARE VARIABLE CONTAJURO VARCHAR(60);
   DECLARE VARIABLE CONTACOMISSAO integer;  
   DECLARE VARIABLE TOTALIZA Double precision;
+  DECLARE VARIABLE SOMA Double precision;
+  DECLARE VARIABLE SOMAmp Double precision;   
+  DECLARE VARIABLE QTDE Double precision;
   DECLARE VARIABLE TOTALREC Double precision = 0;
   DECLARE VARIABLE DESC_MERCADO Double precision = 0;  
   DECLARE VARIABLE produt VARCHAR(300);
@@ -54,7 +58,7 @@ BEGIN
   SUSPEND;
   TOTALIZA = 0;
   TOTAL = 0;
-  FOR select sum(md.valTotal)    
+  FOR select sum(md.VLR_BASE * md.QUANTIDADE)    
      FROM MOVIMENTO mov 
        inner join MOVIMENTODETALHE md on md.CODMOVIMENTO = mov.CODMOVIMENTO
        inner join VENDA ven on ven.CODMOVIMENTO = mov.CODMOVIMENTO     
@@ -79,9 +83,9 @@ BEGIN
       SUSPEND; 
   end
   TOTALIZA = 0;
-  DESC_MERCADO = TOTAL * 0.1;
+  DESC_MERCADO = 0;--TOTAL * 0.1;
 
-  FOR select sum(md.valTotal)    
+  FOR select sum(md.VLR_BASE * md.QUANTIDADE)    
      FROM MOVIMENTO mov 
        inner join MOVIMENTODETALHE md on md.CODMOVIMENTO = mov.CODMOVIMENTO
        inner join VENDA ven on ven.CODMOVIMENTO = mov.CODMOVIMENTO     
@@ -112,7 +116,7 @@ BEGIN
   CONTA  = null;
   CREDITO = null;
   TOTALIZA = 0;
-  FOR select sum(md.valTotal)    
+  FOR select sum(md.VLR_BASE * md.QUANTIDADE)    
      FROM MOVIMENTO mov 
        inner join MOVIMENTODETALHE md on md.CODMOVIMENTO = mov.CODMOVIMENTO
        inner join VENDA ven on ven.CODMOVIMENTO = mov.CODMOVIMENTO     
@@ -125,7 +129,7 @@ BEGIN
       IF (CREDITO IS NULL) THEN
         CREDITO = 0;
 
-      DESC_CONTA = '    Receitas de Serviços ';
+      DESC_CONTA = '    Receitas de Servicos ';
       TOTALIZA = TOTALIZA + CREDITO;
       TOTAL = TOTAL + TOTALIZA;
       ACUMULA = TOTAL;
@@ -135,7 +139,7 @@ BEGIN
   end  
 
   /*  Receitas Vendas OS */
-  DESC_CONTA = 'Receitas não operacionais';
+  DESC_CONTA = 'Receitas nao operacionais';
   CONTA  = null;
   CREDITO = null;
   TOT = 'S';
@@ -155,7 +159,7 @@ BEGIN
       IF (CREDITO IS NULL) THEN
         CREDITO = 0;
 
-      DESC_CONTA = '    Receitas de Juros e Corr. Monetária ';
+      DESC_CONTA = '    Receitas de Juros e Corr. Monetaria ';
       TOTALIZA = TOTALIZA + CREDITO;
       TOTAL = TOTAL + TOTALIZA;
       ACUMULA = TOTAL;
@@ -221,7 +225,7 @@ BEGIN
 
 
   /* Gerando Total Receita */
-  DESC_CONTA = '(-) Deduções das Receitas';
+  DESC_CONTA = '(-) Deducoes das Receitas';
   CONTA  = null;
   CREDITO = null;
   TOTAL = null;
@@ -256,14 +260,23 @@ BEGIN
   CREDITO = null;
   TOTALIZA = 0;
   TOTAL = ACUMULA;
-  FOR select sum(ven.DESCONTO)     
-     FROM VENDA ven
-     where  (ven.DATAVENDA between :PDTA1 AND :PDTA2) and ((ven.CODCCUSTO = :PCC) OR (:PCC = 0))
+  FOR select sum(r.DESCONTO)     
+     FROM RECEBIMENTO r
+     where  (r.EMISSAO between :PDTA1 AND :PDTA2) 
      INTO :CREDITO
   do
   begin    
       IF (CREDITO IS NULL) THEN
         CREDITO = 0;
+        
+     select sum(r.DESCONTO)     
+     FROM VENDA r
+     where  (r.DATAVENDA between :PDTA1 AND :PDTA2) 
+     INTO :DESC_MERCADO;
+
+     if (DESC_MERCADO IS NULL) THEN 
+       desc_mercado = 0;        
+        
       CREDITO = CREDITO + DESC_MERCADO; 
       DESC_CONTA = '    Descontos Concedidos';
       TOTALIZA = TOTALIZA - CREDITO;
@@ -301,7 +314,7 @@ BEGIN
         SUSPEND; 
   end  
 
-  DESC_CONTA = '= Receitas Líquidas';
+  DESC_CONTA = '= Receitas Liquidas';
   CONTA  = null;
   CREDITO = null;
   --TOTAL = NULL;
@@ -322,51 +335,100 @@ BEGIN
 
 
   /* Total dos produtos VENDIDOS * seu custo  */
-
-  FOR select sum(md.VLR_BASE*md.QUANTIDADE)     
-     FROM MOVIMENTO mov 
-       inner join MOVIMENTODETALHE md on md.CODMOVIMENTO = mov.CODMOVIMENTO
-       inner join COMPRA cP on cp.CODMOVIMENTO = mov.CODMOVIMENTO     
-       inner join PRODUTOS prod on prod.CODPRODUTO = md.CODPRODUTO 
-      where (mov.CODNATUREZA = 4) 
-       and (cp.DATACOMPRA between :PDTA1 AND :PDTA2) 
-       and ((prod.TIPO <> 'SERV') or (prod.TIPO is null)) 
-       and ((cp.CODCCUSTO = :PCC) OR (:PCC = 0))
-    INTO :CREDITO
-    do
-    begin
-      IF (CREDITO IS NULL) THEN
-        CREDITO = 0;
-
-      DESC_CONTA = '     Custo dos Produtos Vendidos';
-      TOTALIZA = TOTALIZA - CREDITO;
-      TOTAL = TOTAL + TOTALIZA;
-      ACUMULA = TOTAL;
-      TOT = 'N';
-      SUSPEND; 
-    end
-  TOTALIZA = 0;
-  FOR select sum(md.QUANTIDADE * md.VLR_BASE)     
+  soma = 0;
+  FOR select md.CODPRODUTO, sum(md.QUANTIDADE)
+     
      FROM MOVIMENTO mov 
        inner join MOVIMENTODETALHE md on md.CODMOVIMENTO = mov.CODMOVIMENTO
        inner join VENDA ven on ven.CODMOVIMENTO = mov.CODMOVIMENTO     
        inner join PRODUTOS prod on prod.CODPRODUTO = md.CODPRODUTO 
-       inner join NATUREZAOPERACAO natu on natu.CODNATUREZA = mov.CODNATUREZA
-     where  (natu.TIPOMOVIMENTO = 3) and (ven.DATAVENDA between :PDTA1 AND :PDTA2) and prod.TIPO = 'SERV'  and ((ven.CODCCUSTO = :PCC) OR (:PCC = 0))
-    INTO :CREDITO
-    do
-    begin
-      IF (CREDITO IS NULL) THEN
-        CREDITO = 0;
-
-      DESC_CONTA = '     Custo dos Serviços Vendidos';
-      TOTALIZA = TOTALIZA - CREDITO;
-      TOTAL = TOTAL + TOTALIZA;
-      ACUMULA = TOTAL; 
-      TOT = 'N';
-      if (CREDITO > 0) then
-        SUSPEND; 
-    end
+     where (mov.codnatureza = 3) 
+       and (ven.DATAVENDA between :PDTA1 AND :PDTA2) 
+       and ((PROD.TIPO <> 'SERV') OR (PROD.TIPO IS NULL)) 
+       and ((ven.CODCCUSTO = :PCC) OR (:PCC = 0))
+       and (mov.STATUS = 0)
+     group by md.CODPRODUTO  
+     into :CodP, :qtde
+  do begin 
+    select ev.CUSTOMEDIO from ESTOQUE_CUSTOMEDIO(:PDTA1, :PDTA2, :codP) ev 
+    into :credito;  
+    if (credito is null) then 
+      credito = 0;
+    --if (credito = 0) then   
+    BEGIN
+      -- nao teve compra deste item, ve se usa materia prima  
+       SELECT SUM(COALESCE(r.QTDEUSADA,0) * 
+       (select ev.CUSTOMEDIO from ESTOQUE_CUSTOMEDIO(:PDTA1, :PDTA2, r.CODPRODMP) ev)) 
+        FROM MATERIA_PRIMA r, PRODUTOS p
+       WHERE r.CODPRODMP  = p.CODPRODUTO 
+         AND r.CODPRODUTO = :codP
+        into :SOMAmp;   
+       if (SOMAmp is null) then 
+           SOMAmp = 0;        
+       if (somamp > 0) then 
+         credito = somamp;        
+    end 
+    soma = soma + (credito*qtde);
+  END
+  credito = soma;
+  IF (CREDITO IS NULL) THEN
+    CREDITO = 0;
+  DESC_CONTA = '     Custo dos Produtos Vendidos';
+  TOTALIZA = TOTALIZA - CREDITO;
+  TOTAL = TOTAL + TOTALIZA;
+  ACUMULA = TOTAL;
+  TOT = 'N';
+  SUSPEND;  
+   
+  TOTALIZA = 0;
+  soma = 0;
+  FOR select md.CODPRODUTO, sum(md.QUANTIDADE)
+     
+     FROM MOVIMENTO mov 
+       inner join MOVIMENTODETALHE md on md.CODMOVIMENTO = mov.CODMOVIMENTO
+       inner join VENDA ven on ven.CODMOVIMENTO = mov.CODMOVIMENTO     
+       inner join PRODUTOS prod on prod.CODPRODUTO = md.CODPRODUTO 
+     where (mov.codnatureza = 3) 
+       and (ven.DATAVENDA between :PDTA1 AND :PDTA2) 
+       and (PROD.TIPO = 'SERV') 
+       and ((ven.CODCCUSTO = :PCC) OR (:PCC = 0))
+       and (mov.STATUS = 0)
+     group by md.CODPRODUTO  
+     into :CodP, :qtde
+  do begin 
+    select ev.CUSTOMEDIO from ESTOQUE_CUSTOMEDIO(:PDTA1, :PDTA2, :codP) ev 
+    into :credito;  
+    if (credito is null) then 
+      credito = 0;
+    --if (credito = 0) then   
+    BEGIN
+      -- nao teve compra deste item, ve se usa materia prima  
+       SELECT SUM(COALESCE(r.QTDEUSADA,0) * 
+       (select ev.CUSTOMEDIO from ESTOQUE_CUSTOMEDIO(:PDTA1, :PDTA2, r.CODPRODMP) ev)) 
+        FROM MATERIA_PRIMA r, PRODUTOS p
+       WHERE r.CODPRODMP  = p.CODPRODUTO 
+         AND r.CODPRODUTO = :codP
+        into :SOMAmp;   
+       if (SOMAmp is null) then 
+           SOMAmp = 0;        
+       if (somamp > 0) then 
+         credito = somamp;        
+    end 
+    soma = soma + (credito*qtde);
+  END
+  credito = soma;
+  IF (CREDITO IS NULL) THEN
+    CREDITO = 0;
+  DESC_CONTA = '     Custo dos Produtos Vendidos';
+  TOTALIZA = TOTALIZA - CREDITO;
+  TOTAL = TOTAL + TOTALIZA;
+  ACUMULA = TOTAL;
+  TOT = 'N';
+  SUSPEND;  
+   
+  
+  
+  
   TOTALIZA = 0;
 
   -- Outros Custos de Vendas (FRETE , SEGUROS, etc )
@@ -498,7 +560,7 @@ BEGIN
         IF (CREDITO IS NULL) THEN
           CREDITO = 0;
 
-        DESC_CONTA = '    Despesas Juros e Corr. Monetária ';
+        DESC_CONTA = '    Despesas Juros e Corr. Monetaria ';
         TOTALIZA = TOTALIZA - CREDITO;
         TOTAL = TOTAL + TOTALIZA;
         ACUMULA = TOTAL;
@@ -520,7 +582,7 @@ BEGIN
   TOTAL = ACUMULA + TOTALIZA;
 
   /* Resultado Geral do Período */
-  DESC_CONTA = ' Resultado Geral no período : ';
+  DESC_CONTA = ' Resultado Geral no periodo : ';
   CONTA  = null;
   CREDITO = null;
   
