f1ddfc27868fb658b360c3d4859bd8042cd94840
Carlos Silveira <carlos@atsti.com.br>
2014-10-03 10:03:44 -0300

    Arrumado ..

--- script/extrato_pag.sql
+++ script/extrato_pag.sql
@@ -1,3 +1,4 @@
+Set term ^ ;
 CREATE OR ALTER PROCEDURE EXTRATO_PAG (
     DTAINI Date,
     DTAFIM Date,
@@ -10,15 +11,15 @@ RETURNS (
     DTAVENC Date,
     DTAPAGTO Date,
     CODFORN Integer,
-    FORNECEDOR Varchar(60),
-    DESCRICAO Varchar(150),
-    CONTADESPESA Varchar(200),
+    FORNECEDOR Varchar(100),
+    DESCRICAO Varchar(200),
+    CONTADESPESA Varchar(300),
     VALORTITULO Double precision,
     VALORPAGO Double precision,
     SALDO Double precision,
-    CAIXA Varchar(60),
-    CODCONTA Varchar(15),
-    TITULO VARCHAR(25) )
+    CAIXA Varchar(200),
+    CODCONTA Varchar(25),
+    TITULO VARCHAR(35) )
 AS
 DECLARE VARIABLE CCAIXA INTEGER;
 DECLARE VARIABLE CCONTABIL INTEGER;
@@ -28,7 +29,7 @@ BEGIN
   valorpago = 0;
   /* Lista os lancamentos a pagar */
   FOR SELECT pag.DATAVENCIMENTO, pag.CODFORNECEDOR, forn.NOMEFORNECEDOR, 
-    sum(pag.valor_RESTO), pag.CONTACREDITO, pag.TITULO, pag.EMISSAO
+    sum(pag.valor_RESTO), pag.CONTACREDITO, pag.TITULO, pag.EMISSAO, sum(coalesce(pag.VALORRECEBIDO,0))
     FROM PAGAMENTO pag, FORNECEDOR forn where forn.CODFORNECEDOR = pag.CODFORNECEDOR 
     and pag.DATAVENCIMENTO BETWEEN :DTAINI AND :DTAFIM 
     and ((pag.CODFORNECEDOR = :cod_for) or (:cod_for = 9999999))
@@ -38,24 +39,25 @@ BEGIN
      group by pag.CODFORNECEDOR, forn.NOMEFORNECEDOR, pag.DATAVENCIMENTO,  
        pag.CONTACREDITO, pag.TITULO, pag.EMISSAO
     order by pag.CODFORNECEDOR, pag.DATAVENCIMENTO
-  INTO  :DTAVENC, :CODFORN, :FORNECEDOR, :VALORTITULO, :CCONTABIL, :TITULO, :DTAVENC
+  INTO  :DTAVENC, :CODFORN, :FORNECEDOR, :VALORTITULO, :CCONTABIL, :TITULO, :DTAEMISSAO, :VALORPAGO
   DO BEGIN
     FORNECEDOR = CAST(:CODFORN AS VARCHAR(5)) || '-' ||  :FORNECEDOR;
-    SELECT NOME, CODREDUZIDO FROM PLANO WHERE CODIGO = :CCONTABIL
+    SELECT COALESCE(CODREDUZIDO, '0') || '-' || NOME, COALESCE(CODREDUZIDO, '0') FROM PLANO WHERE CODIGO = :CCONTABIL
     INTO :CONTADESPESA, :CODCONTA;
-    CONTADESPESA = CODCONTA || '-' || CONTADESPESA;
+    -- CONTADESPESA = CODCONTA || '-' || UDF_TRIM(CONTADESPESA);
+   
     SELECT NOME FROM PLANO WHERE CODIGO = :CCAIXA
     INTO :CAIXA;
     
-    SALDO = SALDO + VALORTITULO;
+    SALDO = VALORTITULO - valorPago;
       
-    SUSPEND;
-    descricao = null;
+    --SUSPEND;
+    /*descricao = null;
     ccontabil = null;
     caixa     = null;
-    valorTitulo = 0;
+    valorTitulo = 0;*/
     /* Exibe os descontos feitos por lançamento */
-    FOR SELECT pag.DATAPAGAMENTO, pag.CODFORNECEDOR, forn.NOMEFORNECEDOR, 
+    /*FOR SELECT pag.DATAPAGAMENTO, pag.CODFORNECEDOR, forn.NOMEFORNECEDOR, 
       pag.HISTORICO, pag.VALORRECEBIDO, pag.CONTACREDITO
       FROM PAGAMENTO pag, FORNECEDOR forn where forn.CODFORNECEDOR = pag.CODFORNECEDOR 
       and pag.DATAVENCIMENTO BETWEEN :DTAINI AND :DTAFIM 
@@ -68,15 +70,17 @@ BEGIN
        order by pag.CODFORNECEDOR, pag.DATAPAGAMENTO
     INTO  :DTAPAGTO, :CODFORN, :FORNECEDOR, :DESCRICAO, :VALORPAGO, :CCONTABIL
     DO BEGIN
-    
+      FORNECEDOR = CAST(:CODFORN AS VARCHAR(5)) || '-' ||  :FORNECEDOR;
       SALDO = SALDO - VALORPAGO;
     
-      SUSPEND;
+      
+      
+    end*/     
+    SUSPEND;
       descricao = null;
       ccontabil = null;
       caixa     = null;
       valorpago = 0;
-    end     
     --SALDO = 0;
     
   END
