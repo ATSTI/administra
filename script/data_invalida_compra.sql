4b2742f3c5cdf8ea418c596bcc23eb85c9afd98a
Carlos Silveira <carlos@atsti.com.br>
2014-12-30 13:59:17 -0200

    Colocado bloqueio para Exclusão tbém.

--- script/data_invalida_compra.sql
+++ script/data_invalida_compra.sql
@@ -1,9 +1,30 @@
 CREATE OR ALTER TRIGGER data_invalida_compra FOR COMPRA
-ACTIVE BEFORE INSERT OR UPDATE POSITION 0
+ACTIVE BEFORE INSERT OR UPDATE OR DELETE POSITION 0
 AS 
 BEGIN 
+  IF (INSERTING) then 
+  begin 
 	IF (new.DATACOMPRA < (current_date - 250)) then 
 	  exception data_invalida;
 	IF (new.DATAVENCIMENTO < (current_date - 250)) then 
 	  exception data_invalida;	  
+  end 
+  IF (UPDATING) then 
+  begin 
+	IF (new.DATACOMPRA < (current_date - 250)) then 
+	  exception data_invalida;
+	IF (new.DATAVENCIMENTO < (current_date - 250)) then 
+	  exception data_invalida;	  
+	IF (old.DATACOMPRA < (current_date - 250)) then 
+	  exception data_invalida;
+	IF (old.DATAVENCIMENTO < (current_date - 250)) then 
+	  exception data_invalida;	  
+  end 
+  IF (UPDATING) then 
+  begin  
+	IF (old.DATACOMPRA < (current_date - 150)) then 
+	  exception data_invalida;
+	IF (old.DATAVENCIMENTO < (current_date - 150)) then 
+	  exception data_invalida;	  
+  end   	  
 END
--- script/data_invalida_venda.sql
+++ script/data_invalida_venda.sql
@@ -1,9 +1,31 @@
 CREATE OR ALTER TRIGGER data_invalida_venda FOR VENDA
-ACTIVE BEFORE INSERT OR UPDATE POSITION 0
+ACTIVE BEFORE INSERT OR UPDATE OR DELETE POSITION 0
 AS 
 BEGIN 
+  IF (INSERTING) then 
+  begin 
 	IF (new.DATAVENDA < (current_date - 250)) then 
 	  exception data_invalida;
 	IF (new.DATAVENCIMENTO < (current_date - 250)) then 
 	  exception data_invalida;	  
+  end 
+  IF (UPDATING) then 
+  begin 
+	IF (new.DATAVENDA < (current_date - 250)) then 
+	  exception data_invalida;
+	IF (new.DATAVENCIMENTO < (current_date - 250)) then 
+	  exception data_invalida;	  
+	IF (old.DATAVENDA < (current_date - 250)) then 
+	  exception data_invalida;
+	IF (old.DATAVENCIMENTO < (current_date - 250)) then 
+	  exception data_invalida;	  
+  end 
+  IF (UPDATING) then 
+  begin  
+	IF (old.DATAVENDA < (current_date - 150)) then 
+	  exception data_invalida;
+	IF (old.DATAVENCIMENTO < (current_date - 150)) then 
+	  exception data_invalida;	  
+  end   	  
+	   
 END
