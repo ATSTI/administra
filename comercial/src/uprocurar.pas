23434d3e661c49ebd5619d2a2a45370aa0181c79
Carlos Silveira <carlos@atsti.com.br>
2015-01-05 20:31:36 -0200

    Adicionado tbem a busca por fornecedor.

--- comercial/src/uprocurar.pas
+++ comercial/src/uprocurar.pas
@@ -25,6 +25,7 @@ type
     rep: TVCLReport;
     RadioGroup1: TRadioGroup;
     RadioGroup2: TRadioGroup;
+    BitBtn1: TBitBtn;
     procedure DBGrid1TitleClick(Column: TColumn);
     procedure BtnProcurarClick(Sender: TObject);
     procedure FormShow(Sender: TObject);
@@ -35,6 +36,7 @@ type
     procedure RadioGroup1Click(Sender: TObject);
     procedure RadioGroup2Click(Sender: TObject);
     procedure DBGrid1DblClick(Sender: TObject);
+    procedure BitBtn1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -56,7 +58,7 @@ var
 implementation
 
 uses UDM, uFornecedorCadastro, ufuncionario, uClienteCadastro,
-  uClientePlano, UDmSaude;
+  uClientePlano, UDmSaude, uClienteFiltro;
 
 {$R *.dfm}
 
@@ -217,6 +219,121 @@ procedure TfProcurar.DBGrid1DblClick(Sender: TObject);
 begin
    if ((BtnOk.Enabled = True) and (BtnOk.Visible = True)) then
      BtnOk.Click;
+end;
+
+procedure TfProcurar.BitBtn1Click(Sender: TObject);
+var sForn, varCondicao: String;
+begin
+  fClienteFiltro.Caption := 'Filtro Fornecedor';
+  fClienteFiltro.GroupBox3.Caption := 'Fornecedor';
+  fClienteFiltro.GroupBox2.Caption := 'Comprador';
+  fClienteFiltro.GroupBox5.Visible := False;
+  fClienteFiltro.GroupBox8.Visible := False;
+
+  fClienteFiltro.ShowModal;
+  varCondicao := '';
+  sForn := ' select cli.CODFORNECEDOR, cli.NOMEFORNECEDOR, cli.RAZAOSOCIAL, ende.DDD, ' +
+    ' ende.TELEFONE,  cli.PRAZOPAGAMENTO, cli.CFOP, ende.UF , cli.CODFISCAL ' +
+    '  from FORNECEDOR cli ' +
+    '  left outer join ENDERECOFORNECEDOR ende on ende.CODFORNECEDOR = cli.CODFORNECEDOR ' +
+    ' where ((cli.NOMEFORNECEDOR like :pFORNECEDOR) ' +
+    '    or (cli.RAZAOSOCIAL like :pRAZAO) or (cli.CODFORNECEDOR = :pCODFORNECEDOR)) ' +
+    '   and  (cli.status = :pStatus) ' +
+    '   and  (((cli.segmento = :pSegmento) or (cli.segmento = 1) ) or (:pSegmento = 1 )) ' +
+    '   and ( (ende.TIPOEND = 0) or (ende.TIPOEND is null) ) ';
+
+ if (fClienteFiltro.codVen.Text <> '') then
+   varCondicao := varCondicao + ' and cli.CODUSUARIO = ' + fClienteFiltro.codVen.Text;
+//********************************************************************************************
+ //if (fClienteFiltro.NomeVen.Text <> '') then
+ //  varCondicao := varCondicao + ' and UDF_COLLATEBR(usu.NOMEUSUARIO) containing ' + '''' + fClienteFiltro.nomeVen.Text +  '''';
+//********************************************************************************************
+ if (fClienteFiltro.CBox3.Checked = true) then
+   varCondicao := varCondicao + ' and ((ende.e_mail is not null) and (ende.e_mail <> ' +
+     QuotedStr('') + '))';
+//********************************************************************************************
+ if (fClienteFiltro.CBox4.Checked = true) then
+    varCondicao := varCondicao + ' and ((ende.e_mail = ' + QuotedStr('') + ')or(ende.e_mail is null)) ';
+//********************************************************************************************
+ if (fClienteFiltro.CBox1.Checked = true) then
+   varCondicao := varCondicao + ' and cli.STATUS = 1 ';
+//********************************************************************************************
+ if (fClienteFiltro.CBox2.Checked = true) then
+   varCondicao := varCondicao + ' and cli.STATUS = 2 ';
+  //********************************************************************************************
+
+ if (fClienteFiltro.cbfisica.Checked = true) then
+    varCondicao := varCondicao + ' and cli.TIPOFIRMA = 0 ';
+//********************************************************************************************
+ if (fClienteFiltro.cbjuridica.Checked = true) then
+   varCondicao := varCondicao + ' and cli.TIPOFIRMA = 1 ';
+  //********************************************************************************************
+
+  if (fClienteFiltro.JvDatePickerEdit2.Checked = true) then
+  begin
+    varCondicao := varCondicao + ' and cli.DATACADASTRO between ';
+    varCondicao := varCondicao + QuotedStr(FormatDateTime('mm/dd/yyyy', fClienteFiltro.JvDatePickerEdit2.Date));
+    varCondicao := varCondicao +  ' and ' + QuotedStr(FormatDateTime('mm/dd/yyyy', fClienteFiltro.JvDatePickerEdit3.Date));
+  end;
+//********************************************************************************************
+ if (fClienteFiltro.edCodigo.Text <> '') then
+   varCondicao := varCondicao + ' and cli.CODFORNECEDOR = ' + fClienteFiltro.edCodigo.Text;
+//********************************************************************************************
+ if (fClienteFiltro.edNome.Text <> '') then
+   varCondicao := varCondicao + ' and UDF_COLLATEBR(cli.NOMEFORNECEDOR) containing ' + '''' + fClienteFiltro.edNome.Text +  '''';
+//********************************************************************************************
+ if (fClienteFiltro.edCNPJCPF.Text <> '') then
+   varCondicao := varCondicao + ' and UDF_DIGITS(cli.CNPJ) containing ' + '''' + fClienteFiltro.edCNPJCPF.Text +  '''';
+//********************************************************************************************
+ if (fClienteFiltro.edIERG.Text <> '') then
+   varCondicao := varCondicao + ' and UDF_DIGITS(cli.INSCESTADUAL) containing ' + '''' + fClienteFiltro.edIERG.Text +  '''';
+//********************************************************************************************
+ if (fClienteFiltro.edRazao.Text <> '') then
+   varCondicao := varCondicao + ' and UDF_COLLATEBR(cli.RAZAOSOCIAL) containing ' + '''' + fClienteFiltro.edRazao.Text +  '''';
+//********************************************************************************************
+ if (fClienteFiltro.edCidade.Text <> '') then
+   varCondicao := varCondicao + ' and UDF_COLLATEBR(ende.CIDADE) containing UDF_COLLATEBR(' + '''' + fClienteFiltro.edCidade.Text + '' + ''')';
+//********************************************************************************************
+ if (fClienteFiltro.edtUF.Text <> '') then
+   varCondicao := varCondicao + ' and ende.UF = ' + '''' + (fClienteFiltro.edtUF.Text) +  '''';
+//********************************************************************************************
+ if (fClienteFiltro.edtDDD.Text <> '') then
+   varCondicao := varCondicao + ' and ende.DDD = ' + '''' + fClienteFiltro.edtDDD.Text +  '''';
+//********************************************************************************************
+ //if (fClienteFiltro.cbbRegiao.Text <> '') then
+ //begin
+ //  fClienteFiltro.cdsRegiao.Locate('DESCRICAO', fClienteFiltro.cbbRegiao.Text, [loCaseInsensitive]);
+ //  varCondicao := varCondicao + ' and cli.REGIAO = ' + IntToStr(fClienteFiltro.cdsRegiaoCODDADOS.asInteger);
+ // end;
+//********************************************************************************************
+ { if (fClienteFiltro.cbAniversario.ItemIndex > 0) then
+  begin
+    if (dm.cadastroClienteTipo = 'SIMPLES') then
+    begin
+      varCondicao := varCondicao + ' and UDF_MONTH(cli.DATANASC) =   ';
+      varCondicao := varCondicao + IntToStr(fClienteFiltro.cbAniversario.ItemIndex) +
+       ' and datanasc > ' + QuotedStr('01/01/1930');
+    end;
+  end;}
+//********************************************************************************************
+
+  sForn := sForn + varCondicao + ' order by cli.NOMEFORNECEDOR';
+  dm.scds_forn_proc.Close;
+  dm.scds_forn_proc.CommandText := sForn;
+  dm.scds_forn_proc.Params.CreateParam(ftString, 'pFORNECEDOR', ptInput);
+  dm.scds_forn_proc.Params.CreateParam(ftString, 'pRAZAO', ptInput);
+  dm.scds_forn_proc.Params.CreateParam(ftInteger, 'pCODFORNECEDOR', ptInput);
+  dm.scds_forn_proc.Params.CreateParam(ftSmallint, 'pStatus', ptInput);
+  dm.scds_forn_proc.Params.CreateParam(ftSmallint, 'pSegmento', ptInput);
+  dm.scds_forn_proc.Params.ParamByName('pStatus').AsInteger := 1;
+  dm.scds_forn_proc.Params.ParamByName('pSegmento').AsInteger := 0;
+  BtnProcurar.Click;
+  EvDBFind1.SetFocus;
+  fClienteFiltro.GroupBox3.Caption := 'Cliente';
+  fClienteFiltro.GroupBox2.Caption := 'Vendedor';
+  fClienteFiltro.GroupBox5.Visible := True;
+  fClienteFiltro.GroupBox8.Visible := True;
+  fClienteFiltro.Caption := 'Filtro Cliente';
 end;
 
 end.
