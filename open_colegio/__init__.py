371ed7f5bb9db67d2f3a677d6db30239c4a38862
Carlos Silveira <carlos@atsti.com.br>
2015-03-18 20:04:21 -0300

    Controlando o open colegio.

---
+++ open_colegio/__init__.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+#/#############################################################################
+#
+#    Tech-Receptives Solutions Pvt. Ltd.
+#    Copyright (C) 2004-TODAY Tech-Receptives(<http://www.tech-receptives.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#/#############################################################################
+import controllers
+import op_standard
+import op_route
+import op_faculty
+import op_admission
+import op_hostel_room
+import op_attendance_register
+import op_category
+import op_attendance_sheet
+import op_exam
+import op_exam_media
+import op_result_template
+import op_publisher
+import op_religion
+import op_attendance_line
+import op_transportation
+import op_book_movement
+import op_marksheet_register
+import op_classroom
+import op_vehicle
+import op_hostel
+import op_division
+import op_exam_attendees
+import op_student
+import op_book
+import op_book_queue
+import op_batch
+import op_marksheet_line
+import op_course
+import op_subject
+import op_tag
+import op_result_line
+import op_author
+import op_exam_type
+import op_facility
+import op_allocat_division
+import op_roll_number
+import res_partner
+import op_library
+import op_timetable
+import op_assignment
+import op_assignment_sub_line
+import op_assignment_sub_history
+import op_achievement
+import op_achievement_type
+import wizard
+import report
+import res_company
+import op_health
+import op_book_purchase
+import op_placement_offer
+import op_activity
+import op_parent
+import op_exam_res_allocation
+import op_exam_room
+import op_scholarship
+import op_scholarship_type
+import ea_import
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
