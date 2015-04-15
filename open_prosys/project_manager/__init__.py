60ac02d06fdea5a36086d520c146bf7db8bffbf2
Carlos Silveira <carlos@atsti.com.br>
2015-04-15 14:29:05 -0300

    Alteraçoes e correcoes de bugs no sistema.

---
+++ open_prosys/project_manager/__init__.py
@@ -0,0 +1,2 @@
+import project_manager
+import project
---
+++ open_prosys/project_manager/project_manager.py
@@ -0,0 +1,9 @@
+ #-*- coding: utf-8 -*-
+
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+#import pdb
+
+class project_task(osv.Model):
+    _inherit = "project.task"
+
