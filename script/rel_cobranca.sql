a7bea27ef18806263581c7f8a803b4239aab639a
Carlos R. Silveira <ats@atsti.com.br>
2015-08-14 13:36:33 -0300

    Para a Triagro.

--- script/rel_cobranca.sql
+++ script/rel_cobranca.sql
@@ -1,3 +1,4 @@
+set term ^ ;
 CREATE OR ALTER PROCEDURE REL_COBRANCA (
     CODID integer,
     N_COPIAS integer,
@@ -8,7 +9,7 @@ RETURNS (
     CODCLI Integer,
     NOMECLI Varchar(60),
     RAZAO Varchar(60),
-    titulo Varchar(30),
+    titulo Varchar(120),
     DTAVENCIMENTO Date,
     TOTALDEVE Double precision, 
     codMovimento integer,
@@ -18,41 +19,70 @@ RETURNS (
     preco double precision, 
     produto varchar(300),
     codpro varchar(30)
+    
     )
 AS
 DECLARE VARIABLE CODMOV INTEGER;
 DECLARE VARIABLE CODMOVDET INTEGER;
 DECLARE VARIABLE J INTEGER = 0;
 DECLARE VARIABLE TIPOPROD CHAR(7);
-DECLARE VARIABLE VR DOUBLE PRECISION;
+DECLARE VARIABLE TIT VARCHAR(7);
+DECLARE VARIABLE VALOR DOUBLE PRECISION;
 
 BEGIN
-
+   titulo = '';
+   codMov = 0;
+   totaldeve = 0;
+   --Total Geral :
+   SELECT SUM(rec.VALOR_RESTO)
+      FROM RECEBIMENTO rec 
+     WHERE ((rec.codcliente = :codid) or (:codid = 9999999)) and (rec.EMISSAO between :DTA1 and :DTA2) and (rec.DP = 1)
+      into :totaldeve;
+     
     FOR SELECT rec.CODCLIENTE, SUM(rec.VALOR_RESTO), rec.TITULO, ven.DATAVENDA 
-        sum(md.quantidade), sum(md.VLR_BASE*md.QUANTIDADE), p.produto, p.codpro
-        FROM RECEBIMENTO rec 
-        inner join VENDA ven on rec.CODVENDA = ven.CODVENDA
-        inner join movimentodetalhe md on md.CODMOVIMENTO = ven.CODMOVIMENTO 
-        inner join produtos p on p.CODPRODUTO = md.CODPRODUTO
-        WHERE ((rec.codcliente = :codid)or (:codid = 9999999 )) and rec.EMISSAO between :DTA1 and :DTA2 and rec.DP = 1
-        GROUP BY rec.CODCLIENTE, ven.CODMOVIMENTO, rec.TITULO, ven.DATAVENDA , p.produto, p.codpro
-        into :codcli, :TOTALDEVE, :titulo, :datavenda, :quantidade, :valor, :produto, :codpro   
-        DO BEGIN
-                select c.RAZAOSOCIAL, c.NOMECLIENTE, c.CNPJ from CLIENTES c where c.CODCLIENTE = :codcli
-                into :RAZAO, :NOMECLI, :cnpj;
-               
-                if (UDF_DAY(current_date) > 10 ) then
-                begin
-                    DTAVENCIMENTO = UDF_INCMONTH(current_date, 1);
-                end
-                else
-                    DTAVENCIMENTO = current_date;
-                J = 0;
-                WHILE (N_COPIAS > J) do
-                begin
-                    NOMECLI = NOMECLI || CAST(J AS CHAR(1));
-                    SUSPEND; 
-                    J = J + 1; 
-                END
-        END
+      FROM RECEBIMENTO rec 
+      inner join VENDA ven on rec.CODVENDA = ven.CODVENDA
+      WHERE ((rec.codcliente = :codid) or (:codid = 9999999)) and (rec.EMISSAO between :DTA1 and :DTA2) and (rec.DP = 1)
+      GROUP BY rec.CODCLIENTE, ven.CODMOVIMENTO, rec.TITULO, ven.DATAVENDA 
+       into :codcli, :valor, :tit, :datavenda
+      DO BEGIN --1
+         if (titulo = '') then 
+           titulo = tit;
+         else 
+           titulo = titulo || ', ' || tit;  
+         select c.RAZAOSOCIAL, c.NOMECLIENTE, c.CNPJ from CLIENTES c where c.CODCLIENTE = :codcli
+           into :RAZAO, :NOMECLI, :cnpj;
+         
+         /*if (codMov <> codMovimento) then
+         begin -- 2  
+         
+            for select  p.produto, p.codpro, sum(md.quantidade), sum(md.VLR_BASE*md.QUANTIDADE)
+             from movimentodetalhe md 
+            inner join produtos p on p.CODPRODUTO = md.CODPRODUTO        
+            where md.CODMOVIMENTO = :codmovimento
+            group by p.produto, p.codpro 
+             into :produto, :codpro,:quantidade, :valor
+            do begin --3
+           */ 
+               if (UDF_DAY(current_date) > 10 ) then
+               begin
+                 DTAVENCIMENTO = UDF_INCMONTH(current_date, 1);
+               end
+               else
+                  DTAVENCIMENTO = current_date;
+           /*    
+            end -- 3 
+            codMov = codMovimento;
+            
+         END --2*/
+         
+      end --1   
+               J = 0;
+               WHILE (N_COPIAS > J) do
+               begin
+                  NOMECLI = NOMECLI || CAST(J AS CHAR(1));
+                  SUSPEND; 
+                  J = J + 1; 
+               END
+      
 END
