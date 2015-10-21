aebe399c8f1b6823cf09001aa1c67839758289be
Carlos R. Silveira <crsilveira@gmail.com>
2015-10-21 17:07:50 -0200

    Arrumado pra não bloquear ajuste de preco.

--- script/estoque_fechado_compra.sql
+++ script/estoque_fechado_compra.sql
@@ -3,14 +3,19 @@ ACTIVE BEFORE INSERT OR UPDATE OR DELETE POSITION 0
 AS
   declare variable data_estoque date; 
   declare variable data_compra date; 
+  declare variable valor DOUBLE PRECISION;
 BEGIN 
   select first 1 em.MESANO from estoquemes em order by em.MESANO desc 
   	into :data_estoque;
   if (deleting)	then 
+  begin
     data_compra = old.DATACOMPRA;
-  else   
+    valor = old.VALOR;
+  end   
+  else begin    
     data_compra = new.DATACOMPRA;
-     
-  if (data_compra <= data_estoque) then 
+    valor = new.VALOR;
+  end    
+  if ((data_compra <= data_estoque) and (valor > 0)) then 
     exception ESTOQUEFECHADO; 	 
 END
