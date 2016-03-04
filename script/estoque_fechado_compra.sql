e54dcedf9f4b6d3a5e9901b3fd73b40188ab912f
Carlos R. Silveira <crsilveira@gmail.com>
2016-03-04 07:49:15 -0300

    Arrumado para não permitir edição do registro.

--- script/estoque_fechado_compra.sql
+++ script/estoque_fechado_compra.sql
@@ -1,21 +1,41 @@
+set term ^ ;
 CREATE OR ALTER TRIGGER estoque_fechado_compra FOR COMPRA
 ACTIVE BEFORE INSERT OR UPDATE OR DELETE POSITION 0
 AS
   declare variable data_estoque date; 
   declare variable data_compra date; 
-  declare variable valor DOUBLE PRECISION;
+  declare variable qtde DOUBLE PRECISION;
+  declare variable codmov integer;
 BEGIN 
   select first 1 em.MESANO from estoquemes em order by em.MESANO desc 
   	into :data_estoque;
   if (deleting)	then 
   begin
     data_compra = old.DATACOMPRA;
-    valor = old.VALOR;
+    if (data_compra <= data_estoque) then
+       exception ESTOQUEFECHADO; 	 
   end   
-  else begin    
+  if (INSERTING)	then 
+  begin
     data_compra = new.DATACOMPRA;
-    valor = new.VALOR;
+    codmov = new.CODMOVIMENTO;
+    if (data_compra <= data_estoque) then    
+    begin 
+       for select md.QUANTIDADE from MOVIMENTODETALHE md where md.CODMOVIMENTO = :codMov
+       into :qtde
+       do begin 
+         if (qtde > 0) then  
+           exception ESTOQUEFECHADO; 	 
+       end
+    end 
   end    
-  if ((data_compra <= data_estoque) and (valor > 0)) then 
-    exception ESTOQUEFECHADO; 	 
+  if (UPDATING)	then 
+  begin
+    data_compra = old.DATACOMPRA;
+    if (data_compra <= data_estoque) then
+       exception ESTOQUEFECHADO; 	 
+    data_compra = new.DATACOMPRA;
+    if (data_compra <= data_estoque) then
+       exception ESTOQUEFECHADO; 	 
+  end    
 END
