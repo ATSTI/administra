9edf1a5fb97b14d486b902d17ac73bf6571e5151
Carlos R. Silveira <crsilveira@gmail.com>
2019-04-08 18:43:56 -0300

    Update baixa_estoque.sql

--- script/baixa_estoque.sql
+++ script/baixa_estoque.sql
@@ -3,9 +3,10 @@ BEFORE INSERT POSITION 0
 AS
   declare variable tipoMov smallint;
 BEGIN
+  tipoMov = Null;
   select natu.baixaMovimento from movimento mov, NATUREZAOPERACAO natu 
     WHERE mov.CODNATUREZA = natu.codNatureza and mov.CODMOVIMENTO = new.CODMOVIMENTO
   into :tipoMov;
-  if ((tipoMov = 0) or (tipoMov = 1)) then 
-    UPDATE MOVIMENTODETALHE SET BAIXA = 0 WHERE CODMOVIMENTO = NEW.CODMOVIMENTO;
+  --if ((tipoMov = 0) or (tipoMov = 1)) then 
+  UPDATE MOVIMENTODETALHE SET BAIXA = :tipoMov WHERE CODMOVIMENTO = NEW.CODMOVIMENTO;
 END
